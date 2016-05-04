100df674c8b5a07a8357d96056400ee0bca913d5
Tyron <tyron.burkhardt.za@gmail.com>
2016-05-04 16:18:13 +0200

    calories done and added 3 images for badges

---
+++ Methods/caleroies.js
@@ -0,0 +1,81 @@
+// // exports.foodGroups= function(){
+//   var Carbs = "Potato";
+//   var caloriespergram = 0.75;
+//   var statistics = document.getElementById("calorieStats");
+// var CarbsMap = [];
+// CarbsMap.push({
+//   Product: Carbs,
+//   caloriesPerGram: caloriespergram});
+//
+// // console.log(CarbsMap);
+//
+//
+//   var Meat = "Chicken";
+//   var caloriespergram = 2;
+//
+//   var MeatMap=[];
+//   MeatMap.push({
+//     Product: Meat,
+//     caloriesPerGram: caloriespergram
+//   });
+// // console.log(MeatMap);
+//
+//   var Veg = "Broccoli";
+//   var caloriespergram = 0.34;
+//
+//   var VegMap=[];
+//   VegMap.push({
+//     Product: Veg,
+//     caloriesPerGram: caloriespergram
+//   });
+// // console.log(VegMap);
+//
+//   var Sweets = "Coke";
+//   var caloriespergram = 0.42;
+//
+//   var SweetsMap=[];
+//   SweetsMap.push({
+//     Product: Sweets,
+//     caloriesPerGram: caloriespergram
+//   });
+//   // console.log(SweetsMap);
+//   var Fruits = "Apple";
+//   var caloriespergram = 0.52;
+//
+//   var FruitsMap=[];
+//   FruitsMap.push({
+//     Product: Fruits,
+//     caloriesPerGram: caloriespergram
+//   });
+//   // console.log(FruitsMap);
+//
+// var FoodArray =[];
+// for(x = 0; x < CarbsMap.length; x++){
+//
+// FoodArray.push(
+//   {Product: CarbsMap[x].Product, caloriesPerGram:CarbsMap[x].caloriesPerGram},
+//   {Product: MeatMap[x].Product, caloriesPerGram:MeatMap[x].caloriesPerGram},
+//   {Product: VegMap[x].Product, caloriesPerGram: VegMap[x].caloriesPerGram},
+//   {Product: SweetsMap[x].Product, caloriesPerGram:SweetsMap[x].caloriesPerGram},
+//   {Product: FruitsMap[x].Product, caloriesPerGram:FruitsMap[x].caloriesPerGram}
+// );
+// }
+// console.log(FoodArray);
+// // return FoodArray;
+// // };
+//
+// // exports.calories = function(FoodArray){
+//
+// // var gramsAmount = getElementById("gramsAmount");
+// var userFoodSelect = x;
+//
+// if(x === FoodArray.Product){
+//   var totalCalories = (gramsAmount* FoodArray.caloriesPerGram);
+//   statistics.innerHTML = totalCalories;
+//   return totalCalories;
+// }
+// console.log(totalCalories);
+//
+//
+//
+// // };
